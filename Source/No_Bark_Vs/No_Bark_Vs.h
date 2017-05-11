da4d1b3475e7ae097e2cf8a338c90a0905a2b352
love- <love-@DESKTOP-SENAK>
2017-05-11 19:18:42 +1200

    Defined some surface location for hiiting enemy.

--- Source/No_Bark_Vs/No_Bark_Vs.h
+++ Source/No_Bark_Vs/No_Bark_Vs.h
@@ -5,3 +5,12 @@
 #include "Engine.h"
 
 #define WEAPON_TRACE ECC_GameTraceChannel1
+
+
+/** when you modify this, please note that this information can be saved with instances
+* also DefaultEngine.ini [/Script/Engine.PhysicsSettings] should match with this list **/
+#define SURFACE_DEFAULT				SurfaceType_Default
+#define SURFACE_FLESH				SurfaceType1
+#define SURFACE_ENEMYBODY			SurfaceType2
+#define SURFACE_ENEMYHEAD			SurfaceType3
+#define SURFACE_ENEMYLIMB			SurfaceType4
