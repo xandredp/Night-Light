b26843b216a30acfd706c237d1a8d250fd771746
love- <love-@DESKTOP-SENAK>
2017-05-12 11:42:26 +1200

    plugin working in blueprints yeah!

--- Plugins/AI_Plugin/Source/AI_Plugin/AI_Plugin.Build.cs
+++ Plugins/AI_Plugin/Source/AI_Plugin/AI_Plugin.Build.cs
@@ -39,7 +39,10 @@ public class AI_Plugin : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
-				// ... add private dependencies that you statically link with here ...	
+                		// ... add private dependencies that you statically link with here ...	
+                "InputCore", 
+                "EditorStyle",
+		
 			}
 			);
 		
