38d7e3435d649436f59d0150b7910a405d7c0ed9
MDS\Xandre.plo6323 <xduplooy@gmail.com>
2017-05-18 10:57:14 +1200

    Anim Sequence class for Skinny monster

---
+++ Source/No_Bark_Vs/SkinnyMonsterAnimInstance.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "No_Bark_Vs.h"
+#include "SkinnyMonsterAnimInstance.h"
+
+
+void USkinnyMonsterAnimInstance::UpdateAnimationProperties()
+{
+	// Get the pawn for this anim sequence
+	APawn* Pawn = TryGetPawnOwner();
+
+	if (Pawn)
+	{
+		// Update falling speed 
+
+
+		// Update movement speed
+		MovementSpeed = Pawn->GetVelocity().Size();
+	}
+}
---
+++ Source/No_Bark_Vs/SkinnyMonsterAnimInstance.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Animation/AnimInstance.h"
+#include "SkinnyMonsterAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NO_BARK_VS_API USkinnyMonsterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+protected:
+
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	//	bool IsFalling;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		float MovementSpeed;
+	
+	UFUNCTION(BlueprintCallable, Category = "UpdateAnimationProperties")
+		void UpdateAnimationProperties();
+
+
+
+};
