607009740844d94c04f4968b21bf4de3324e77c2
love- <love-@DESKTOP-SENAK>
2017-06-02 18:29:31 +1200

    attack disabled

---
+++ Source/No_Bark_Vs/Monster/TankyMonster.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "No_Bark_Vs.h"
+#include "TankyMonster.h"
+
+
+//AI Wonderting around
+
+
+
---
+++ Source/No_Bark_Vs/Monster/TankyMonster.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Monster.h"
+#include "TankyMonster.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NO_BARK_VS_API ATankyMonster : public AMonster
+{
+	GENERATED_BODY()
+	
+
+private:
+	ATankyMonster();
+
+
+
+};
