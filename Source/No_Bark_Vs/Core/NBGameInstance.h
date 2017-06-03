034411864954f206c1ffd9e3f23ebb9e2cd850ce
love- <love-@DESKTOP-SENAK>
2017-06-04 00:05:15 +1200

    Game instance for loading screen If we have time.

---
+++ Source/No_Bark_Vs/Core/NBGameInstance.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Engine/GameInstance.h"
+#include "NBGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NO_BARK_VS_API UNBGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
