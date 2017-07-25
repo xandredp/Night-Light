63d428c6aeaf06609726881e9a46ba6605924fef
love- <love-@DESKTOP-SENAK>
2017-07-26 00:50:45 +1200

    door cpp added

---
+++ Source/No_Bark_Vs/Assets/Door.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Core/BaseInteractable.h"
+#include "Door.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NO_BARK_VS_API ADoor : public ABaseInteractable
+{
+	GENERATED_BODY()
+	
+
+public:
+	ADoor();
+
+	// Called when the game starts or when spawned
+	virtual void Interact(APlayerController* playerController) override;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	//void DestroyItemOnGround();
+
+	UFUNCTION(BlueprintImplementableEvent)
+		void ToggleDoor();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config")
+		FName ItemID;
+
+	
+	
+};
