99e74937e477dfbfba586f4d822de2cf52f20e61
sena <sena0711@gmail.com>
2017-10-17 14:48:53 +1300

    capsule collision colliding. removed crashing mug

--- Source/No_Bark_Vs/Monsters/Base/NBBaseAI.cpp
+++ Source/No_Bark_Vs/Monsters/Base/NBBaseAI.cpp
@@ -65,9 +65,9 @@ void ANBBaseAI::OnDeath()
 		
 		DetachFromControllerPendingDestroy();
 		/* Disable all collision on capsule */
-		UCapsuleComponent* CapsuleComp = GetCapsuleComponent();
-		GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-		GetCapsuleComponent()->SetCollisionResponseToAllChannels(ECR_Ignore);
+		//UCapsuleComponent* CapsuleComp = GetCapsuleComponent();
+		//GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		//GetCapsuleComponent()->SetCollisionResponseToAllChannels(ECR_Ignore);
 
 		USkeletalMeshComponent* Mesh3P = GetMesh();
 		if (Mesh3P)
